d576c5f0ed9a48e91372daf8a82c9cdf1f63307d
laedmombo <mombomombo@et.esiea.fr>
2021-12-20 18:08:07 +0100

    projet made

--- src/main/java/fr/lernejo/navy_battle/Launcher.java
+++ src/main/java/fr/lernejo/navy_battle/Launcher.java
@@ -1,33 +1,66 @@
 package fr.lernejo.navy_battle;
-import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpHandler;
-import com.sun.net.httpserver.HttpServer;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.net.InetSocketAddress;
 
-import java.util.concurrent.Executors;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.Charset;
+
+import static java.net.http.HttpResponse.*;
 
 public class Launcher {
-    public static void main(String[] args) throws Exception {
-        HttpServer httpServer = HttpServer.create(new InetSocketAddress(Integer.parseInt(args[0])), 0);
-        httpServer.setExecutor(Executors.newSingleThreadExecutor());
 
-        httpServer.createContext("/ping", new HttpHandler() {
-            @Override
-            public void handle(HttpExchange httpExchange) throws IOException {
-                final byte[] out = "Hello".getBytes("UTF-8");
+    public static final int CLIENT_PORT = 8795;
 
-                httpExchange.sendResponseHeaders(200, out.length);
-
-                OutputStream os = httpExchange.getResponseBody();
-                os.write(out);
-                os.close();
+    public static void main( String[] args ) {
+        if (args.length == 1) {
+            LaunchServer(args[0]);
+        } else {
+            if (args.length == 2) {
+                LaunchClient(args[0], args[1]);
+            } else {
+                System.out.println("Invalid number of parameters");
             }
-        });
-        httpServer.start();
+        }
+    }
 
-        System.out.println("HttpServer Test Start!");
+    private static final void LaunchClient( String port, String url ) {
+        HttpClient client = HttpClient.newHttpClient();
+        JSONObject json = new JSONObject();
+        json.put("id", "1");
+        json.put("url", "http://localhost:" + port);
+        json.put("message", "Hello I'm client");
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url + "/api/game/start"))
+                .setHeader("Accept", "application/json")
+                .setHeader("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(json.toString(4)))
+                .build();
+        try {
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            System.out.println(response.body());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void LaunchServer( String portValue ) {
+        int port;
+        try {
+            port = Integer.parseInt(portValue);
+            try {
+                Server server = new Server(port);
+                System.out.println("Server Ready");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Error invalid port : " + portValue);
+        }
     }
 }
-
