2142e2329d34be603a5f7017b4f321ba22825369
Djawad <alphadjawad@gmail.com>
2021-12-22 23:40:23 +0100

    Some Refactorings

--- src/main/java/fr/lernejo/navy_battle/game/GameFireHandler.java
+++ src/main/java/fr/lernejo/navy_battle/game/GameFireHandler.java
@@ -11,52 +11,51 @@ import org.json.JSONObject;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
 
 public final class GameFireHandler implements HttpHandler {
     private final GameBoard _board;
     private final IPlayer _player1;
     private final IPlayer _player2;
-    private IPlayer _currentPlayer;
 
-    public GameFireHandler( GameBoard board ) {
+    public GameFireHandler(GameBoard board) {
         _board = board;
         _player1 = board.getPlayer1();
         _player2 = board.getPlayer2();
-        _currentPlayer = _player1;
     }
 
     @Override
-    public void handle( HttpExchange exchange ) throws IOException {
+    public void handle(HttpExchange exchange) throws IOException {
         URIParser parser = new URIParser(exchange.getRequestURI());
         String cell = parser.GetValueOf("cell");
         String res = "";
-        Shoot hit = _currentPlayer.hit(cell.charAt(0), Integer.parseInt(cell.charAt(1) + ""));
-        switch (hit) {
-            case Miss: {
-                res = "miss";
-                break;
-            }
-            case Hit: {
-                res = "hit";
-                break;
-            }
-            case Sunk: {
-                res = "sunk";
-                break;
-            }
-        }
+        res = getHitResult(cell);
+        JSONObject json = createJSon(res);
+        createResponse(exchange, json);
+    }
+
+    private String getHitResult(String cell) {
+        Shoot hit = _player1.hit(cell.charAt(0), Integer.parseInt(cell.charAt(1) + ""));
+        HashMap<Shoot, String> map = new HashMap<>();
+        map.put(Shoot.Hit, "hit");
+        map.put(Shoot.Sunk, "sunk");
+        map.put(Shoot.Miss, "miss");
+        return map.get(hit);
+    }
+
+    private JSONObject createJSon(String res) {
         JSONObject json = new JSONObject();
         json.put("consequence", res);
         json.put("shipLeft", _player2.hasShipLeft());
-        _currentPlayer = _currentPlayer.name().equals("Player1") ? _player2 : _player1;
-        json.put("Current Player", _currentPlayer.name());
+        json.put("Current Player", _player1.name());
+        return json;
+    }
 
-
+    private void createResponse(HttpExchange exchange, JSONObject json) throws IOException {
         String response = json.toString(4) + "\n" + _board.toString();
         exchange.sendResponseHeaders(200, response.length());
         OutputStream os = exchange.getResponseBody();
         os.write(response.getBytes(StandardCharsets.UTF_8));
         os.close();
-
     }
 }
--- src/main/java/fr/lernejo/navy_battle/game/PingHandler.java
+++ src/main/java/fr/lernejo/navy_battle/game/PingHandler.java
@@ -8,11 +8,9 @@ import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
 
 public class PingHandler implements HttpHandler {
-    private int _count = 0;
 
     @Override
-    public void handle( HttpExchange exchange ) throws IOException {
-        _count++;
+    public void handle(HttpExchange exchange) throws IOException {
         String out = "Hello";
 
         exchange.sendResponseHeaders(200, out.length());
@@ -20,6 +18,5 @@ public class PingHandler implements HttpHandler {
         OutputStream os = exchange.getResponseBody();
         os.write(out.getBytes(StandardCharsets.UTF_8));
         os.close();
-        System.out.println("Number: " + exchange.getRequestMethod());
     }
 }
